75557be48fc89f4e7c28d5a7a49b858f53d8f886
Joel Klaus <jaklaus0101@gmail.com>
2017-08-28 14:14:24 -0500

    Initial commit of node chat app

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
