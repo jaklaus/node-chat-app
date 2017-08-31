62b6f5ff528f9526ec6aef262e38ce84df39815a
Joel Klaus <jaklaus0101@gmail.com>
2017-08-31 17:26:04 -0500

    Create log in messages for new and existing clients

--- server/server.js
+++ server/server.js
@@ -15,14 +15,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log(`New user connected`);
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome a new user to the chat room!',
+    createAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log(`Message:`, message)
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
-  })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
   socket.on('disconnect', () => {
     console.log(`User was disconnected`);
